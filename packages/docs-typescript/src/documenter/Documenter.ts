d7effc16fb1613f87c36a7ed9f42abe0cf3f5212
michalfedyna <michal@fedyna.pl>
2024-03-23 01:50:59 +0100

    feat: update Documenter

--- packages/docs-typescript/src/documenter/Documenter.ts
+++ packages/docs-typescript/src/documenter/Documenter.ts
@@ -3,11 +3,11 @@ import { DocsConfig } from "../config/DocsConfig";
 
 import { Emitter } from "../emitters/Emitter";
 import { MDEmitter } from "../emitters/markdown/MDEmitter";
-import { HTMLEmitter } from "../emitters/html/HTMLEmitter";
-import { MDXEmitter } from "../emitters/mdx/MDXEmitter";
 
 import { RootNode } from "./api/RootNode";
 import { ApiExtractor } from "./ApiExtractor";
+
+import { Debug } from "../utils/Debug";
 
 class Documenter {
 	public readonly apiModel: ApiModel;
@@ -21,26 +21,18 @@ class Documenter {
 		this.apiTree = new RootNode();
 
 		switch (config.outputFormat) {
-			// case "html":
-			// 	this._emitter = new HTMLEmitter(config);
-			// 	break;
-			//
-			// case "markdown":
-			// 	this._emitter = new MDEmitter(config);
-			// 	break;
-			//
-			// case "mdx":
-			// 	this._emitter = new MDXEmitter(config);
-			// 	break;
 			default:
 				this.emitter = new MDEmitter(config);
 		}
 	}
 
 	public emit(): void {
-		// console.log(JSON.stringify(this.apiTree.toObject(), null, 2));
 		ApiExtractor.traverse(this.apiModel, this.apiTree);
 		this.emitter.emit(this.apiTree);
+	}
+
+	public debug(): void {
+		Debug.log(JSON.stringify(this.apiTree.toObject(), null, 2));
 	}
 }
 
